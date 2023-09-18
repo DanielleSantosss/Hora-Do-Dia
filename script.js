9e31f37f6b6fcecb920d8e85f63382eb82debf52
Danielle <danielle.vtds@gmail.com>
2023-09-18 18:51:00 -0300

    ajuste de horas

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ function carregar(){
     var imagem = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    var minutos = data.getMinutes()
     
-    if (hora >= 0 && hora <12) {
-        mensagem.innerHTML = `Bom dia, agora são ${hora} horas!`    
+    if (hora >= 0 && hora < 12) {
+        mensagem.innerHTML = `Bom dia, agora são ${hora}h${minutos}!`    
         imagem.src = 'manha.jpg'
         document.body.style.background = '#e2cd9f'
-    } else if (hora >= 12 && hora <= 18) {
-        mensagem.innerHTML = `Boa tarde, agora são ${hora} horas!`
+    } else if (hora >= 12 && hora < 18) {
+        mensagem.innerHTML = `Boa tarde, agora são ${hora}h${minutos}!`
         imagem.src = 'tarde.jpg'
         document.body.style.background = '#FFB52B'
     } else {
-        mensagem.innerHTML = `Boa noite, agora são ${hora} horas!`
+        mensagem.innerHTML = `Boa noite, agora são ${hora}h${minutos}!`
         imagem.src = 'noite.jpg'
         document.body.style.background = '#000430'
     }
